0550fb40e042d96ed71c9141293b88299203636d
a7madddddd <142698638+a7madddddd@users.noreply.github.com>
2024-10-11 16:40:07 +0300

    adding classes service controller

---
+++ GumAndHealth.Server/DTOs/ClassesDTOs/dasboardClassesCrud.cs
@@ -0,0 +1,14 @@
+﻿namespace GumAndHealth.Server.DTOs.ClassesDTOs
+{
+    public class dasboardClassesCrud
+    {
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public decimal? PricePerMonth { get; set; }
+    }
+}
