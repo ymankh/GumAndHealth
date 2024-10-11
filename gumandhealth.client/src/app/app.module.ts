9fac033e3070c75b9394f758abac757f06dccaab
najlaaalsmadi <najlaak399@gmail.com>
2024-10-11 17:29:05 +0300

    recipes  category

--- gumandhealth.client/src/app/app.module.ts
+++ gumandhealth.client/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { ProfileComponent } from './profile/profile.component';
 import { FormsModule } from '@angular/forms';
 import { TipsComponent } from './nutrition/tips/tips.component';
+import { RecipesComponent } from './nutrition/recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { TipsComponent } from './nutrition/tips/tips.component';
     ClassesComponent,
     ContactComponent,
     ProfileComponent,
-    TipsComponent
+    TipsComponent,
+    RecipesComponent
   ],
   imports: [
     FormsModule,
--- gumandhealth.client/src/app/nutrition/recipes/recipes.component.ts
+++ gumandhealth.client/src/app/nutrition/recipes/recipes.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NajlaaService } from '../../services/najlaa.service'; // استيراد الخدمة
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.css',
-
-
+  styleUrls: ['./recipes.component.css']
 })
-export class RecipesComponent {
-  viewRecipeDetails(id: number) {}
+export class RecipesComponent implements OnInit {
+  recipeCategories: any[] = []; // استخدام 'any' بدلاً من واجهة معينة
+
+  constructor(private najlaaService: NajlaaService) { }
+
+  // تنفيذ عند تحميل الصفحة
+  ngOnInit(): void {
+    this.najlaaService.getRecipeCategories().subscribe(categories => {
+      this.recipeCategories = categories; // تخزين الفئات المسترجعة في المصفوفة
+    });
+  }
+
+  // دالة لاستعراض التفاصيل (اختياري)
+  viewRecipeDetails(id: number): void {
+    console.log(`Viewing details for recipe category with ID: ${id}`);
+  }
 }
