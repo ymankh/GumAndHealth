d72c2593aa8fa9b805165034bdd6d68b9dbc2a9b
Yaman Khashashneh <ymankh1997@gmail.com>
2024-10-11 09:11:00 +0300

    confeg the static files

--- GumAndHealth.Server/Program.cs
+++ GumAndHealth.Server/Program.cs
@@ -1,6 +1,10 @@
 
+using System.Text;
 using GumAndHealth.Server.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
 
 namespace GumAndHealth.Server
 {
@@ -14,6 +18,51 @@ namespace GumAndHealth.Server
             // Add services to the container.
             builder.Services.AddDbContext<MyDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("YourConnectionString")));
+            // Add JWT Bearer Authentication
+            builder.Services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false; // Set to true for production if using HTTPS
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                });
+            builder.Services.AddAuthorization();
+
+
+            // CORS configuration to allow any origin, method, and header
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAnyOrigin",
+                    corsPolicyBuilder =>
+                    {
+                        corsPolicyBuilder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+            // Add services to the container.
+            builder.Services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                // Handle circular references
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+                // Alternatively, you can use Preserve instead of Ignore:
+                // options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+                // options.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,7 +72,13 @@ namespace GumAndHealth.Server
             var app = builder.Build();
 
             app.UseDefaultFiles();
-            app.UseStaticFiles();
+            app.UseStaticFiles(); // Serve wwwroot
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(builder.Environment.ContentRootPath, "images")),
+                RequestPath = "/images"
+            });
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
