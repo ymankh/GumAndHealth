4c57d4ede341bf190489b19aca75a74e0853373b
a7madddddd <142698638+a7madddddd@users.noreply.github.com>
2024-10-11 23:59:31 +0300

    display classes in admin dashboard

--- gumandhealth.client/src/app/services/ahmad.service.ts
+++ gumandhealth.client/src/app/services/ahmad.service.ts
@@ -3,18 +3,26 @@ import { Injectable } from '@angular/core';
 import { root } from '../shared/constants';
 import { Observable } from 'rxjs';
 
+
+
+
+
 @Injectable({
   providedIn: 'root',
 })
 export class AhmadService {
   constructor(private http: HttpClient) { }
-
-
-
   
 
   addContact(data: any): Observable<any> {
     debugger
     return this.http.post<any>("https://localhost:44325/api/Contacts", data)
   }
+
+
+
+  ///////////////////////////////////
+  
+
+
 }
