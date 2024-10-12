837a6f921a35ccc6d1f9347a9b75e34c17471a45
Yaman Khashashneh <ymankh1997@gmail.com>
2024-10-12 07:19:32 +0300

    add cart service

--- gumandhealth.client/src/app/shared/interfaces.ts
+++ gumandhealth.client/src/app/shared/interfaces.ts
@@ -5,6 +5,39 @@ export interface Category {
   imagePath: string;
   recipes: Recipe[];
 }
+export interface CartItem {
+  id: number;
+  productId: number;
+  cartId?: number;
+  quantity: number;
+  product?: Product;
+  cart?: Cart;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+export interface Cart {
+  id: number;
+  userId: number;
+  cartItems?: CartItem[];
+  user: User;
+}
+
+export interface User {
+  email: string;
+  image?: string;
+  status?: string;
+  createdAt: Date;
+  // addresses: Address[];
+  carts: Cart[];
+  // classSubscriptions: ClassSubscription[];
+  // gymSubscriptions: GymSubscription[];
+  // orders: Order[];
+}
 
 export interface Recipe {
   id: number;
